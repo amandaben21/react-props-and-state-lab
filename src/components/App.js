e8dc06ace246e58adc5013fc160e9a8315323934
Amanda <amandaben21@aol.com>
2021-07-04 00:38:36 -0400

    DONE!

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
-
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
-
 class App extends React.Component {
   constructor() {
     super()
@@ -15,6 +13,37 @@ class App extends React.Component {
     }
   }
 
+  handleChangeFilterType = (type) => {
+    this.setState({
+      filters: Object.assign({}, this.state.filters,{   //Object.assign{} already returns target object
+        type: type
+      })
+    })
+  }
+
+  getThePets = () => {
+    let url = '/api/pets';
+    if(this.state.filters.type !== 'all') {
+      url += `?type=${this.state.filters.type}`
+    }
+
+    fetch(url)
+      .then(res => res.json())
+      .then(pets => this.setState({pets}));
+  }
+
+  handleAdoptPet = petId => {
+    let petsToAdopt = this.state.pets;
+    const adoptedPet = petsToAdopt.find(pet => pet.id === petId);
+
+    adoptedPet.isAdopted = true;
+
+    this.setState({
+      pets: petsToAdopt
+    });
+
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +53,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters filters={this.filters} onchangeType={this.handleChangeFilterType} onFindPetsClick={this.getThePets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} adpotedPets={this.state.adoptedPets} onAdoptPet={this.handleAdoptPet}/>
             </div>
           </div>
         </div>
